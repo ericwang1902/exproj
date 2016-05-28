99394d84423cca01c69717e61f905659471c2afd
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2016-05-28 17:35:45 +0800

    增加注册页面的表单验证。

--- routes/index.js
+++ routes/index.js
@@ -26,13 +26,36 @@ router.post('/login',function (req,res,next) {
       res.render('./contents/login',{errors:errors});
     }else{
       console.log('表单没错')
-      
+      //进行数据库的登录操作
     }
     
 })
 
 router.get('/register',function(req,res,next){
   res.render('./contents/register');
+});
+
+router.post('/register',function(req,res,next){
+  console.log(req.body);
+  var username = req.body.username;
+  var password = req.body.password1;
+  var password2 = req.body.password2;
+  
+  req.checkBody('username','用户名不可为空').notEmpty();
+  req.checkBody('password','密码不可为空').notEmpty();
+  req.checkBody('password2','请重复输入密码').notEmpty();
+  
+  var errors = req.validationErrors();
+  
+  if(errors){
+    console.log('注册表单有错')
+    res.render('./contents/register',{errors:errors});
+  }else{
+    console.log('表单没错')
+    //需要进行数据库的注册操作
+  }
+  
+  //res.render('./contents/register');
 });
 
 module.exports = router;
