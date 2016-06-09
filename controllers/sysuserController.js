bfa8c3a2c1385b6d5556b7d6ebc37f5d2a62a2f3
王海军 <EricWang@promote.cache-dns.local>
2016-06-09 22:04:49 +0800

    自主注册，callback的用法。

--- controllers/sysuserController.js
+++ controllers/sysuserController.js
@@ -1,5 +1,5 @@
 var sysuserModel = require('../models/sysuserModel.js');
-
+var bcrypt = require('bcryptjs');
 /**
  * sysuserController.js
  *
@@ -41,12 +41,64 @@ module.exports = {
         });
     },
 
+    //用户注册，创建用户
+    createUser:function(user,callback){
+        var sysuser = new sysuserModel({
+            mobile:user.username,
+            psd:user.psd,
+            usertype : "",
+            openid : "",
+            count : 0,
+            type : "",
+            account : "",
+            accountpsd : "",
+            //orgid : "",
+            groupid : "",
+            status : "",
+            isbroadcast : ""
+        });
+        
+        sysuserModel.findOne({mobile:user.username},function(err,user){
+            if(err) return console.error(err);
+            
+            if(user){
+                console.log('改号码已被占用！')
+            }else{
+                console.log('可以创建！')
+                bcrypt.genSalt(10,function(err,salt){
+                   bcrypt.hash(sysuser.psd,salt,function(err,hash){
+                       sysuser.psd = hash;
+                       
+                       sysuser.save(function(err,user){
+                           if(err) return console.error(err);
+                          // console.log(user);
+                           callback(null,user);
+                       })
+                   })
+                })
+            }
+            
+        })
+    },
     /**
      * sysuserController.create()
      */
     create: function(req, res) {
-        var sysuser = new sysuserModel({			mobile : req.body.mobile,			psd : req.body.psd,			usertype : req.body.usertype,			openid : req.body.openid,			count : req.body.count,			type : req.body.type,			account : req.body.account,			accountpsd : req.body.accountpsd,			orgid : req.body.orgid,			groupid : req.body.groupid,			status : req.body.status,			isbroadcast : req.body.isbroadcast
+        var sysuser = new sysuserModel({
+			mobile : req.body.mobile,
+			psd : req.body.psd,
+			usertype : req.body.usertype,
+			openid : req.body.openid,
+			count : req.body.count,
+			type : req.body.type,
+			account : req.body.account,
+			accountpsd : req.body.accountpsd,
+			orgid : req.body.orgid,
+			groupid : req.body.groupid,
+			status : req.body.status,
+			isbroadcast : req.body.isbroadcast
         });
+        
 
         sysuser.save(function(err, sysuser){
             if(err) {
@@ -80,7 +132,19 @@ module.exports = {
                 });
             }
 
-            sysuser.mobile =  req.body.mobile ? req.body.mobile : sysuser.mobile;			sysuser.psd =  req.body.psd ? req.body.psd : sysuser.psd;			sysuser.usertype =  req.body.usertype ? req.body.usertype : sysuser.usertype;			sysuser.openid =  req.body.openid ? req.body.openid : sysuser.openid;			sysuser.count =  req.body.count ? req.body.count : sysuser.count;			sysuser.type =  req.body.type ? req.body.type : sysuser.type;			sysuser.account =  req.body.account ? req.body.account : sysuser.account;			sysuser.accountpsd =  req.body.accountpsd ? req.body.accountpsd : sysuser.accountpsd;			sysuser.orgid =  req.body.orgid ? req.body.orgid : sysuser.orgid;			sysuser.groupid =  req.body.groupid ? req.body.groupid : sysuser.groupid;			sysuser.status =  req.body.status ? req.body.status : sysuser.status;			sysuser.isbroadcast =  req.body.isbroadcast ? req.body.isbroadcast : sysuser.isbroadcast;			
+            sysuser.mobile =  req.body.mobile ? req.body.mobile : sysuser.mobile;
+			sysuser.psd =  req.body.psd ? req.body.psd : sysuser.psd;
+			sysuser.usertype =  req.body.usertype ? req.body.usertype : sysuser.usertype;
+			sysuser.openid =  req.body.openid ? req.body.openid : sysuser.openid;
+			sysuser.count =  req.body.count ? req.body.count : sysuser.count;
+			sysuser.type =  req.body.type ? req.body.type : sysuser.type;
+			sysuser.account =  req.body.account ? req.body.account : sysuser.account;
+			sysuser.accountpsd =  req.body.accountpsd ? req.body.accountpsd : sysuser.accountpsd;
+			sysuser.orgid =  req.body.orgid ? req.body.orgid : sysuser.orgid;
+			sysuser.groupid =  req.body.groupid ? req.body.groupid : sysuser.groupid;
+			sysuser.status =  req.body.status ? req.body.status : sysuser.status;
+			sysuser.isbroadcast =  req.body.isbroadcast ? req.body.isbroadcast : sysuser.isbroadcast;
+			
             sysuser.save(function(err, sysuser){
                 if(err) {
                     return res.json(500, {
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var bcrypt = require('bcryptjs');
 var sysuserModel = require('../models/sysuserModel');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var sysuserController = require('../controllers/sysuserController')
 
 /* GET home page. */
 router.get('/',isLogedIn,function(req, res, next) {
@@ -30,14 +31,28 @@ router.get('/register',function(req,res,next){
   res.render('./contents/register');
 });
 
-router.post('/register',function(req,res,next){
-  console.log(req.body);
+router.post('/register',checkRegForm,function(req,res,next){
+  console.log('通过表单验证')
+  //创建用户账号
+  var user = {
+    username:req.body.username,
+    psd:req.body.password1
+  }
+  sysuserController.createUser(user,function(err,result){
+    console.log(result);
+  });//创建用户
+  
+});
+
+//表单验证中间件
+function checkRegForm(req,res,next) {
+    console.log(req.body);
   var username = req.body.username;
-  var password = req.body.password1;
+  var password1 = req.body.password1;
   var password2 = req.body.password2;
   
   req.checkBody('username','用户名不可为空').notEmpty();
-  req.checkBody('password','密码不可为空').notEmpty();
+  req.checkBody('password1','密码不可为空').notEmpty();
   req.checkBody('password2','请重复输入密码').notEmpty();
   
   var errors = req.validationErrors();
@@ -47,11 +62,10 @@ router.post('/register',function(req,res,next){
     res.render('./contents/register',{errors:errors});
   }else{
     console.log('表单没错')
-    //需要进行数据库的注册操作
+    next();//需要进行数据库的注册操作
+    
   }
-  
-  //res.render('./contents/register');
-});
+}
 
 //做路由登陆验证
 function isLogedIn(req,res,next){
