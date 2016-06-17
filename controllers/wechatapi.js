acfc39457986d5d54ec7f8d6072f5df926f810fb
王海军 <EricWang@promote.cache-dns.local>
2016-06-17 23:34:46 +0800

    debug

--- controllers/wechatapi.js
+++ controllers/wechatapi.js
@@ -2,7 +2,7 @@ var WechatAPI = require('wechat-api');
 var enumberableconstants =require('../models/enumerableConstants');
 var api = new WechatAPI(enumberableconstants.wechatinfo.appid, enumberableconstants.wechatinfo.appsecret);//需要修改
 
-api.setOpts({timeout: 15000});
+//api.setOpts({timeout: 15000});
 api.getLatestToken(function (err,token) {
     console.log('token:'+JSON.stringify(token));
 });
