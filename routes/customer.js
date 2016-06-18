afb91206ae2665b6248fc9bfb45bb1481b0aeb78
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2016-06-18 16:07:07 +0800

    测试

--- routes/customer.js
+++ routes/customer.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var wechatjs = require('../controllers/wechatapi');//调用wechatjs来设置
 var request =require('request');
 var enumerableconstants = require('../models/enumerableConstants')
+var async = require('async');
 
 router.get('/order1',function (req,res,next) {
     res.render('./customer/order1',{layout: false});
@@ -38,18 +39,46 @@ router.post('/location',function(req,res,next){
 })
 
 function getjssdktoken(req,res,next){
-    console.log(req.query.code);
+    console.log(req.query.code);//获取微信重定向之后，生成的code
 
-    var options={
-       url: 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='+enumerableconstants.wechatinfo.appid+'&secret='+enumerableconstants.wechatinfo.appsecret+'&code='+req.query.code+'&grant_type=authorization_code'
-    }
 
-    request(options,function (error,response,body) {   
-            console.log('error:'+error);  
+    
+    async.waterfall([
+    //获取accesstoken
+    function(callback) {
+        var accesstokenoptions={
+        url: 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='+enumerableconstants.wechatinfo.appid+'&secret='+enumerableconstants.wechatinfo.appsecret+'&code='+req.query.code+'&grant_type=authorization_code'
+        }
+        request(accesstokenoptions,function (error,response,body) {  
+            var access_token = body.access_token;
+            var refresh_token = body.refresh_token;
+            var openid=body.openid;
+               
+        })
+        callback(null, access_token, refresh_token,openid);
+    },
+    //获取用户信息
+    function(access_token, refresh_token,openid, callback) {
+        var userinfooptions={
+            url: 'https://api.weixin.qq.com/sns/userinfo?access_token='+access_token+'&openid='+openid+'&lang=zh_CN'
+        }
+        request(userinfooptions,function (err,response,body) {
+            console.log('err:'+err);
             console.log('response:'+response);
             console.log('body:'+body);
-           
-    })
+        })
+        callback(null, 'three');
+    },
+    function(arg1, callback) {
+        // arg1 now equals 'three'
+        callback(null, 'done');
+    }
+], function (err, result) {
+    // result now equals 'done'
+    
+});
+    
+    
 
     return next();
 }
