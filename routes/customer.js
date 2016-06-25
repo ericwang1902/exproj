84064b9596eee8dc4588017c2c65b7ca3eb9547c
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2016-06-26 07:47:44 +0800

    debug

--- routes/customer.js
+++ routes/customer.js
@@ -84,7 +84,7 @@ router.post('/location',function(req,res,next){
 
 
 router.get('/loclist',function(req,res,next){
-    res.render('./customer/order1');
+    res.render('./customer/loclist');
 })
 
 
