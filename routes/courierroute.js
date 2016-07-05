5b213645d91131bc2b1d4995d114d5ed3f049cf6
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2016-07-05 17:20:14 +0800

    debug

--- routes/courierroute.js
+++ routes/courierroute.js
@@ -220,7 +220,7 @@ router.post('/pickupdateorder',function(req,res,next){
         },
         function(order,callback){
             //发送模板消息
-            wechatjs.sendTemplate(openid,enumerableconstants.wechatinfo.templateId2,'http://exproj.robustudio.com/',order.ordercode,'状态','张三','1231414',function(err,result){})
+            wechatjs.sendTemplate2(openid,enumerableconstants.wechatinfo.templateId2,'http://exproj.robustudio.com/',order.ordercode,'状态','张三','1231414',function(err,result){})
             
         }
     ],function(err,result){
