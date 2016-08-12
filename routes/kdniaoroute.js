0a44341ef763a97181c9c9228fdae8cdc4c257f2
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2016-08-12 17:24:09 +0800

    debug

--- routes/kdniaoroute.js
+++ routes/kdniaoroute.js
@@ -6,8 +6,9 @@ var enumerableconstants = require('../models/enumerableConstants')
 var moment = require('moment')
 
 router.post('/bookorder',function (req,res,next) {
-    console.log('bookorder:'+req.body);
+    console.log('bookorder:'+JSON.stringify(req.body));
     //解析trace，寻找到运单，存储近order的trace
+    
     
     var result={
     "EBusinessID": enumerableconstants.kdniao.businessid,
