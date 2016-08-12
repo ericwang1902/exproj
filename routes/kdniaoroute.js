4d2271c002c3ff7d6593e99fd6096f41930a7e43
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2016-08-12 17:09:36 +0800

    debug

--- routes/kdniaoroute.js
+++ routes/kdniaoroute.js
@@ -7,13 +7,8 @@ router.post('/bookorder',function (req,res,next) {
     console.log('bookorder:'+req.body);
     //解析trace，寻找到运单，存储近order的trace
     
-    var result={
-    "EBusinessID": "1256928",
-    "UpdateTime": "2015-3-11 16:26:11",
-    "Success": true,
-    "Reason":""
-    }
-    res.json(result);
+
+    res.send('test')
 
 })
 
