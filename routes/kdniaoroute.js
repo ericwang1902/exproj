02086872fa28d9fa516386d2fee5082cdc8f3ff7
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2016-08-15 17:03:49 +0800

    debug

--- routes/kdniaoroute.js
+++ routes/kdniaoroute.js
@@ -12,7 +12,8 @@ var enumerableConstants = require('../models/enumerableConstants');
 var kdniao = require('../controllers/kdniao')
 
 router.post('/bookorder',function (req,res,next) {
-    
+            
+            console.log('请求来了');
             console.log('打印：'+JSON.stringify(JSON.parse(req.body)));
            // console.log('反序列化：'+JSON.stringify(JSON.parse(req.body)));
             var LogisticDataArray= JSON.parse(req.body).Data.RequestData;
