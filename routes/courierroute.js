f13cf178d98a52c11d1fff07c87df910ae66b44c
ericwang1902 <ericwang1902@163.com>
2016-08-29 10:42:11 +0800

    debug

--- routes/courierroute.js
+++ routes/courierroute.js
@@ -419,11 +419,15 @@ function getuserinfo(req,res,next){
     }
 ], function (err, result) {
     // result now equals 'done'
-    console.log(result);
-    var userinfoJson = JSON.parse(result);
-    req.userinfoJson = userinfoJson;
-     
-     return next();
+        console.log(result);
+
+        if(result){
+            var userinfoJson = JSON.parse(result);
+            req.userinfoJson = userinfoJson;
+            return next();
+        }else{
+            getuserinfo();
+        }
 });
     
    
