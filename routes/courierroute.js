4e4ca80986ce8e7a3f4427e419a8106d200e97c5
ericwang1902 <ericwang1902@163.com>
2016-09-01 11:01:38 +0800

    debug

--- routes/courierroute.js
+++ routes/courierroute.js
@@ -363,7 +363,7 @@ router.post('/pickupdateorder', function (req, res, next) {
 function getuserinfo(req, res, next) {
 
     //添加判断openid是否存在该属性
-    if (!req.query) {
+    if (Object.prototype.hasOwnProperty.call(req, 'query') && !req.query) {
         var queryurl = req.query;
         var queryobj = qs.parse(queryurl);
         var querydata = Object.create(queryobj)
