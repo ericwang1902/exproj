b6ddf22614f04326e5f7cf3ac35c60b3ba0c993c
ericwang1902 <ericwang1902@163.com>
2016-09-05 10:54:16 +0800

    封装socketio

--- routes/socketio.js
+++ routes/socketio.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var app = express();//获取app对象
+var socketio={}
+var socket_io = require('socket.io')
 
-var io = require('socket.io')(app);
+socketio.getSocketio = function(server){
+  var io = socket_io.listen(server);
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });//触发news事件
-  
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-})
+  io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });//触发news事件
+    
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
+  })
+};
+
+module.exports = socketio;
