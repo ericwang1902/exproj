bf5aad7b53cf3317887293d7380a28df20a357fc
ericwang1902 <ericwang1902@163.com>
2016-09-05 11:15:59 +0800

    debug

--- routes/orgroutes.js
+++ routes/orgroutes.js
@@ -58,7 +58,6 @@ router.get('/orguserlist',isLogedIn, function (req, res, next) {
 
 router.get('/orguserdetail',isLogedIn, function (req, res, next) {
     var id = req.query.id;//快递员id
-io.sendinfo();
     //根据快递员id查找到他所归属的快递点，读取快递点的信息。
     async.waterfall([
         function (callback) {
