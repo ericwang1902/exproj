1b25eeb60bfdc88413fc00eb53a006f7fc758f40
ericwang1902 <ericwang1902@163.com>
2016-09-06 13:14:44 +0800

    debug

--- controllers/sysuserController.js
+++ controllers/sysuserController.js
@@ -332,7 +332,7 @@ module.exports = {
                     .find({"orgid":org._id})
                     .exec(function(err,users){
                         orgs[index].children = users;
-                        console.log(JSON.stringify(orgs))
+                        console.log(JSON.stringify(orgs[index]))
                         callback();
                     }
                     )
