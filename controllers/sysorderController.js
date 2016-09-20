97c8ed8a64290cf88be364f963ecec35a7d60765
ericwang1902 <ericwang1902@163.com>
2016-09-20 21:53:17 +0800

    var orderArray1 =new Array(24);

--- controllers/sysorderController.js
+++ controllers/sysorderController.js
@@ -534,7 +534,7 @@ module.exports = {
     },
     //获取所有订单在全天各时段的下单分布
     getHourOrderAll: function (callback1) {
-        var orderArray1 =[];
+        var orderArray1 =new Array(24);
         async.forEachOf(
                 orderArray1,
                 function(dataItem,key,callback){
